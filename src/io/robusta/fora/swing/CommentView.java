9741ec3d5950defc38097a790009b3bd205a4369
jmv <jean_marc.villatte@aliceadsl.fr>
2014-01-31 16:04:46 +0100

    second commit

---
+++ src/io/robusta/fora/swing/CommentView.java
@@ -0,0 +1,19 @@
+package io.robusta.fora.swing;
+
+import javax.swing.JPanel;
+
+public class CommentView extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public CommentView() {
+
+	}
+
+}
